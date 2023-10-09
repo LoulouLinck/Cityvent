c95320d67395c2cc27a49b55625062d85b7f5283
LoulouLinck <136842839+LoulouLinck@users.noreply.github.com>
2023-10-10 01:56:35 +0200

    Create handler.js

---
+++ auth-server/handler.js
@@ -0,0 +1,121 @@
+'use strict'; //added by default when generate serverless template code
+ 
+const { google } = require("googleapis"); //required Google package 
+const calendar = google.calendar("v3"); //required Google Calendar
+const SCOPES = ["https://www.googleapis.com/auth/calendar.events.public.readonly"]; // set access levels (was configuerd in Google console as "read-only-access")
+const { CLIENT_SECRET, CLIENT_ID, CALENDAR_ID } = process.env;
+const redirect_uris = [
+  "https://louloulinck.github.io/Cityvent/"
+];
+
+const oAuth2Client = new google.auth.OAuth2(
+  CLIENT_ID,
+  CLIENT_SECRET,
+  redirect_uris[0]
+);
+
+//SERVERLESS FUNCTION 1
+module.exports.getAuthURL = async () => {
+  /**
+   *
+   * Scopes array is passed to the `scope` option. 
+   *
+   */
+  const authUrl = oAuth2Client.generateAuthUrl({
+    access_type: "offline",
+    scope: SCOPES,
+  });
+
+  return {
+    statusCode: 200,
+    headers: {
+      'Access-Control-Allow-Origin': '*',
+      'Access-Control-Allow-Credentials': true,
+    },
+    body: JSON.stringify({
+      authUrl,
+    }),
+  };
+};
+
+//SERVERLESS FUNCTION 2
+module.exports.getAccessToken = async (event) => {
+  // Decode authorization code extracted from the URL query
+  const code = decodeURIComponent(`${event.pathParameters.code}`);
+
+  return new Promise((resolve, reject) => {
+    /**
+     *  Exchange authorization code for access token with a “callback” after the exchange,
+     *  The callback in this case is an arrow function with the results as parameters: “error” and “response”
+     */
+
+    oAuth2Client.getToken(code, (error, response) => {
+      if (error) {
+        return reject(error);
+      }
+      return resolve(response);
+    });
+  })
+    .then((results) => {
+      // Respond with OAuth token 
+      return {
+        statusCode: 200,
+        headers: {
+          'Access-Control-Allow-Origin': '*',
+          'Access-Control-Allow-Credentials': true,
+        },
+        body: JSON.stringify(results),
+      };
+    })
+    .catch((error) => {
+      // Handle error
+      return {
+        statusCode: 500,
+        body: JSON.stringify(error),
+      };
+    });
+};
+
+//SERVERLESS FUNCTION 3
+module.exports.getCalendarEvents = async (event) => {
+  // accesses token
+  const access_token = decodeURIComponent(`${event.pathParameters.access_token}`);
+  // sets credentials
+  oAuth2Client.setCredentials({ access_token });
+  // gets calendar events
+  return new Promise((resolve, reject) => {
+    calendar.events.list(
+      {
+        calendarId: CALENDAR_ID,
+        auth: oAuth2Client,
+        timeMin: new Date().toISOString(),
+        singleEvents: true,
+        orderBy: "startTime",
+      },
+    (error, response) => {
+      if (error) {
+        return reject(error);
+      }
+      return resolve(response);
+    }
+    );
+  })
+    .then((results) => {
+      // Returns OAuth token
+      return {
+        statusCode: 200,
+        headers: {
+          'Access-Control-Allow-Origin': '*',
+          'Access-Control-Allow-Credentials': true,
+        },
+        body: JSON.stringify({ events: results.data.items }),
+      };
+    })
+    .catch((error) => {
+      // Handle error
+      return {
+        statusCode: 500,
+        body: JSON.stringify(error),
+      };
+    });
+};
