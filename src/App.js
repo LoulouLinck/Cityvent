3e51e7ed0a89d3e858698c5dff522b6a03ca03d9
Laure Lincker <136842839+LoulouLinck@users.noreply.github.com>
2023-10-20 20:16:08 +0200

    implemented code to know the selected suggestion
    as a way to know what the current selected suggestion is:
    
    -created new state representing the value of the queried local state that’s in CitySearch.js once the user selects one of the suggestion items
    -update fetchData() so that it automatically filters out the list of events based on the value of the currentCity state, in the case that it contains a value other than "See all cities", otherwise, all events will be rendered
    -make sure that fetchData() is called whenever there’s a change in the currentCity state, simply use it as a dependency in the useEffect()

--- src/App.js
+++ src/App.js
@@ -13,13 +13,17 @@ const App = () => {
   const [events, setEvents] = useState([]);
   const [currentNOE, setCurrentNOE] = useState(32);
   const [allLocations, setAllLocations] = useState([]);
+  const [currentCity, setCurrentCity] = useState("See all cities");
   
   useEffect(() => {
     fetchData();
-  }, []);
+  }, [currentCity]);
 
   const fetchData = async () => {
     const allEvents = await getEvents();
+    const filteredEvents = currentCity === "See all cities" ?
+      allEvents :
+      allEvents.filter(event => event.location === currentCity)
     setEvents(allEvents.slice(0, currentNOE));
     setAllLocations(extractLocations(allEvents));
 }
