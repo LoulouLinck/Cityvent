e5beefd2f3ebd690c85773aed7ebb66b2b860fac
Laure Lincker <136842839+LoulouLinck@users.noreply.github.com>
2023-10-20 17:16:51 +0200

    created new scope for integration test

--- src/App.js
+++ src/App.js
@@ -1,8 +1,17 @@
+import { useEffect, useState } from 'react';
+
 import CitySearch from './components/CitySearch';
 import EventList from './components/EventList';
 // import Event from './components/Event';
 import NumberOfEvents from './components/NumberOfEvents';
+
+import { getEvents } from './api';
+
 import './App.css';
+
+
+
+
 
 
 const App = () => {
