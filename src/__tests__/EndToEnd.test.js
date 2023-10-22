5bc6ee76f7bfb86c7dac24aa7ff0385901c0fde5
Laure Lincker <136842839+LoulouLinck@users.noreply.github.com>
2023-10-23 00:37:59 +0200

    wrote end-to-end tests for Feature 1

--- src/__tests__/EndToEnd.test.js
+++ src/__tests__/EndToEnd.test.js
@@ -1,6 +1,6 @@
 import puppeteer from 'puppeteer';
 
-describe('show/hide event details', () => {
+describe('Feature: Show/Hide Event Details', () => {
   let browser;
   let page;
   beforeAll(async () => {
@@ -36,5 +36,59 @@ describe('show/hide event details', () => {
     await page.click('.event-item .details-btn');
     const eventDetails = await page.$('.event-item .event-details');
     expect(eventDetails).toBeNull();
+  });
+});
+
+
+
+describe('Feature: Filter Events By City', () => {
+  let browser;
+  let page;
+
+  beforeAll(async () => {
+    browser = await puppeteer.launch({ headless: false, slowMo: 250, timeout: 0 });
+    page = await browser.newPage();
+    await page.goto('http://localhost:3000/');
+    await page.waitForSelector('.event-item');
+  });
+
+  afterAll(() => {
+    browser.close();
+  });
+
+  test('When user hasn`t searched for a city, show upcoming events from all cities', async () => {
+    const events = await page.$$('.event-item');
+    expect(events.length).toBe(32); // Assuming there are 32 events by default
+  });
+
+  test('User should see a list of suggestions when they search for a city', async () => {
+    await page.type('.city-search .city-input', 'Berlin');
+    const suggestionsOnPage = await page.$$('.city-search .suggestions .suggestion-item');
+    expect(suggestionsOnPage.length).toBeGreaterThan(0);
+  });
+
+  test('User can select a city from the suggested list', async () => {
+    await page.evaluate(() => document.querySelector('.city-search .city-input').value = '');
+    await page.type('.city-search .city-input', 'Berlin');
+
+    // Wait for the suggestions to be loaded
+    await page.waitForSelector('.city-search .suggestions .suggestion-item');
+
+    // Get all suggestion items
+    const suggestions = await page.$$('.city-search .suggestion-item');
+
+    // Check if there are enough suggestions to click on the second one
+    if (suggestions.length > 1) {
+      await suggestions[0].click(); // Click on the second suggestion
+    } else {
+      throw new Error('Not enough suggestions found');
+    }
+
+    const inputValue = await page.$eval('.city-search .city-input', input => input.value);
+    expect(inputValue).toBe('Berlin, Germany');
+
+    const events = await page.$$('.event-item');
+    // You would need to verify the number of events displayed matches the expected number for Berlin, Germany
+    expect(events.length).toBe(17);
   });
 });
