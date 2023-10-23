abd5af4782608f2e0c986a091672ef2faacec41b
Laure Lincker <136842839+LoulouLinck@users.noreply.github.com>
2023-10-23 11:45:01 +0200

    imported getEvents from correct module
    imported function from '../mock-data' instead of '../api'

--- src/features/filterEventsByCity.test.js
+++ src/features/filterEventsByCity.test.js
@@ -2,7 +2,7 @@ import userEvent from '@testing-library/user-event';
 import { loadFeature, defineFeature } from 'jest-cucumber';
 import { render,  within, waitFor } from '@testing-library/react';
 import App from '../App';
-import { getEvents } from '../mock-data';
+import { getEvents } from '../api';
 
 const feature = loadFeature('./src/features/filterEventsByCity.feature');
 
