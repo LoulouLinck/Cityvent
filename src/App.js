377d154da7bac56c8eed5af23d936ed93dc58f97
Laure Lincker <136842839+LoulouLinck@users.noreply.github.com>
2023-10-28 02:40:13 +0200

    created warning alert

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import CityEventsChart from './components/CityEventsChart';
 import EventGenresChart from './components/EventGenresChart';
 import { InfoAlert } from './components/Alert';
 import { ErrorAlert } from './components/Alert';
+import { WarningAlert } from "./components/Alert";
 
 import { extractLocations, getEvents } from './api';
 
@@ -20,8 +21,16 @@ const App = () => {
   const [currentCity, setCurrentCity] = useState("See all cities");
   const [infoAlert, setInfoAlert] = useState("");
   const [errorAlert, setErrorAlert] = useState("");
-  
+  const [warningAlert, setWarningAlert] = useState("");
+
   useEffect(() => {
+    if (navigator.onLine) {
+      setWarningAlert("");
+    } else {
+      setWarningAlert(
+        "No internet connection: the page you're viewing might not be updated."
+      );
+    }
     fetchData();
   }, [currentCity, currentNOE]);
 
@@ -36,22 +45,25 @@ const App = () => {
 
   return (
     <div className="App">
-      <h1>Cityvent</h1>
+      <header className="App-header">  
+       <h1>Cityvent</h1>
+      <div className='searchBars'>
+            <CitySearch allLocations={allLocations}  setCurrentCity={setCurrentCity} setInfoAlert={setInfoAlert} />
+      <NumberOfEvents setCurrentNOE={setCurrentNOE} setErrorAlert={setErrorAlert} />
+      </div>
+      </header>
       <div className="alerts-container">
         {infoAlert.length ? <InfoAlert text={infoAlert}/> : null}
         {errorAlert.length ? <ErrorAlert text={errorAlert}/> : null}
+        {warningAlert.length ? <WarningAlert text={warningAlert} /> : null}
       </div>
-     <CitySearch allLocations={allLocations}  setCurrentCity={setCurrentCity} 
-        setInfoAlert={setInfoAlert} />
-     <NumberOfEvents setCurrentNOE={setCurrentNOE} setErrorAlert={setErrorAlert} />
-     <CityEventsChart allLocations={allLocations} events={events} />
       
      <div className="charts-container">
        <CityEventsChart allLocations={allLocations} events={events} />
        <EventGenresChart events={events} />
      </div>
      <EventList events={events} />
-    </div>
+   </div>
   );
 }
 
